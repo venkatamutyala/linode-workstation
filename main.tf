3b0fcfd8b3d60b9c07ea794969bb2c1d54a6301c
Venkata Mutyala <venkata@venkatamutyala.com>
2022-04-16 18:16:45 -0700

    wip

--- main.tf
+++ main.tf
@@ -1,16 +1,20 @@
-# resource "linode_instance" "web" {
-#     label = "simple_instance"
-#     image = "linode/ubuntu20.04"
-#     region = "us-southeast"
-#     type = "g7-highmem-1"
-#     authorized_keys = ["ssh-rsa AAAA...Gw== user@example.local"]
-#     root_pass = "terr4form-test"
+locals {
+    authorized_keys = compact(split("\n",data.http.example.body))
+}
 
-#     group = "foo"
-#     tags = [ "foo" ]
-#     swap_size = 256
-#     private_ip = true
-# }
+resource "linode_instance" "web" {
+    label = "venkata-workstation"
+    image = "linode/ubuntu20.04"
+    region = "us-southeast"
+    type = "g7-highmem-1"
+    authorized_keys = local.authorized_keys
+
+
+    group = "foo"
+    tags = [ "foo" ]
+    swap_size = 256
+    private_ip = true
+}
 
 
 data "http" "example" {
@@ -19,8 +23,4 @@ data "http" "example" {
     Accept = "text/plain"
   }
 
-}
-
-output "keys" {
-    value = compact(split("\n",data.http.example.body))
 }

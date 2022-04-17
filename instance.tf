cc2aec32eee9e7878e61b92c0024736b1aceb6a5
Venkata Mutyala <venkata@venkatamutyala.com>
2022-04-16 22:09:27 -0700

    wip

--- instance.tf
+++ instance.tf
@@ -1,45 +1,5 @@
 
 
-
-resource "linode_stackscript" "bootstrap" {
-  label = "bootstrap"
-  description = "Installs all core software"
-  script = <<EOF
-#!/bin/bash -e
-set -x
-sudo apt-get update
-sudo apt-get -y install zsh
-sudo apt-get -y install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io
-chsh -s $(which zsh)
-useradd -m -s /bin/zsh venkatamutyala
-sudo usermod -aG sudo venkatamutyala
-sudo usermod -aG docker venkatamutyala
-sudo touch /home/venkatamutyala/.zshrc
-sudo cp -r ~/.ssh /home/venkatamutyala/
-chown -R venkatamutyala:venkatamutyala /home/venkatamutyala
-
-sudo -i -u venkatamutyala zsh << EOF2
-cd /home/venkatamutyala
-git clone https://github.com/asdf-vm/asdf.git /home/venkatamutyala/.asdf
-curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | zsh
-sed -i 's/plugins=(git)/plugins=(git asdf)/' /home/venkatamutyala/.zshrc
-EOF2
-
-EOF
-  images = ["linode/ubuntu20.04"]
-  rev_note = "initial version"
-}
-
 resource "linode_instance" "venkata-workstation" {
     label = "venkata-workstation"
     image = "linode/ubuntu20.04"

f3cbdf17785e7b2080464574d104e419513abc88
Venkata Mutyala <venkata@venkatamutyala.com>
2022-04-16 20:18:26 -0700

    wip

--- dns.tf
+++ dns.tf
@@ -3,7 +3,7 @@ data "linode_domain" "yolo_venkatamutyala_com" {
 }
 
 resource "linode_domain_record" "venkata-workstation" {
-    domain_id = linode_domain.yolo_venkatamutyala_com.id
+    domain_id = data.linode_domain.yolo_venkatamutyala_com.id
     record_type = "A"
     target = "${linode_instance.venkata-workstation.ip_address}"
     ttl_sec = local.ttl

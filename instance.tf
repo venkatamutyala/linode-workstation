94982a593602823c091d322930040f3ebfa59ae1
Venkata Mutyala <venkata@venkatamutyala.com>
2022-04-16 22:14:01 -0700

    wip

--- instance.tf
+++ instance.tf
@@ -1,5 +1,20 @@
 
 
+
+resource "linode_stackscript" "bootstrap" {
+  label = "bootstrap"
+  description = "Installs all core software"
+  script = <<EOF
+#!/bin/bash
+cd
+echo "curl https://raw.githubusercontent.com/venkatamutyala/linode-workstation/main/setup.sh | bash" > bootstrap.sh
+chmod +x bootstrap.sh
+
+EOF
+  images = ["linode/ubuntu20.04"]
+  rev_note = "initial version"
+}
+
 resource "linode_instance" "venkata-workstation" {
     label = "venkata-workstation"
     image = "linode/ubuntu20.04"
@@ -7,6 +22,7 @@ resource "linode_instance" "venkata-workstation" {
     type = "g7-highmem-1"
     authorized_keys = local.authorized_keys
 
+    stackscript_id = linode_stackscript.bootstrap.id
     swap_size = 256
     private_ip = true
 }

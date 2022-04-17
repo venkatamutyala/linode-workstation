8f80ac5372d2a14fd617d173ec917718631d3d02
Venkata Mutyala <venkata@venkatamutyala.com>
2022-04-16 18:33:43 -0700

    wip

--- main.tf
+++ main.tf
@@ -6,12 +6,14 @@ resource "linode_domain" "yolo_venkatamutyala_com" {
     type = "master"
     domain = "yolo.venkatamutyala.com"
     soa_email = "venkata@venkatamutyala.com"
+    ttl_sec = 30
 }
 
 resource "linode_domain_record" "venkata-workstation" {
     domain_id = linode_domain.yolo_venkatamutyala_com.id
     record_type = "A"
     target = "${linode_instance.venkata-workstation.ip_address}"
+    ttl_sec = 30
 }
 
 resource "linode_instance" "venkata-workstation" {

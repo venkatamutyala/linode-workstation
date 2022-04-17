c03f1aad1f070e0417fab03efd317cb76b908f6b
Venkata Mutyala <venkata@venkatamutyala.com>
2022-04-17 00:26:54 -0700

    wip

--- instance.tf
+++ instance.tf
@@ -9,7 +9,7 @@ data "linode_images" "ubuntu" {
 
 resource "linode_instance" "venkata-workstation" {
     label = "venkata-workstation"
-    image = data.linode_images.ubuntu[0].id
+    image = data.linode_images.ubuntu.id
     region = "us-southeast"
     type = "g7-highmem-1"
     authorized_keys = local.authorized_keys

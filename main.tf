8a50d02d39613400e8f0ef3de257e5e94fed966b
Venkata Mutyala <venkata@venkatamutyala.com>
2022-04-16 18:38:16 -0700

    wip

--- main.tf
+++ main.tf
@@ -1,22 +1,23 @@
 locals {
     authorized_keys = compact(split("\n",data.http.example.body))
+    ttl = 30
 }
 
 resource "linode_domain" "yolo_venkatamutyala_com" {
     type = "master"
     domain = "yolo.venkatamutyala.com"
     soa_email = "venkata@venkatamutyala.com"
-    ttl_sec = 0
-    retry_sec = 0
-    expire_sec = 0
-    refresh_sec = 0
+    ttl_sec = local.ttl
+    retry_sec = local.ttl
+    expire_sec = local.ttl
+    refresh_sec = local.ttl
 }
 
 resource "linode_domain_record" "venkata-workstation" {
     domain_id = linode_domain.yolo_venkatamutyala_com.id
     record_type = "A"
     target = "${linode_instance.venkata-workstation.ip_address}"
-    ttl_sec = 0
+    ttl_sec = local.ttl
 }
 
 resource "linode_instance" "venkata-workstation" {

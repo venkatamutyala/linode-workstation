9ce6f78f162de0126350b93a1de388553408cc23
Venkata Mutyala <venkata@venkatamutyala.com>
2022-04-24 04:36:09 +0000

    feat: adding k8s

---
+++ k8s.tf
@@ -0,0 +1,15 @@
+resource "linode_lke_cluster" "venkata-test-1" {
+  label       = "venkata-test-1"
+  k8s_version = "1.23"
+  region      = "us-southeast"
+
+  pool {
+    type  = "g7-highmem-1"
+    count = 3
+
+    autoscaler {
+      min = 1
+      max = 3
+    }
+  }
+}
